48ffdc6e7dc645f41253e05b22d859a0ea5da9cb
Alexandr Kozlinskiy <akozlins@gmail.com>
2012-10-21 13:25:31 +0200

    first working version

--- hook_x86_dll.c
+++ hook_x86_dll.c
@@ -17,6 +17,8 @@ HINSTANCE hinst_ = 0;
 
 void _log_(const char* fmt, ...)
 {
+  return;
+
   FILE* file = fopen("d:/out.txt", "a+");
   if(!file) return;
 
@@ -31,8 +33,8 @@ void _log_(const char* fmt, ...)
 typedef struct {
   HWND hwnd;
 //  WNDPROC fproc;
-  RECT rect;
-  RECT rect_d;
+  int l, r, t, b;
+  int dl, dr, dt, db;
 } wnd_node;
 
 wnd_node wnds[16] = { 0 };
@@ -43,10 +45,91 @@ wnd_node* wnd_find(HWND hwnd)
   return 0;
 }
 
+BOOL CALLBACK fenum(HWND hwnd, LPARAM lp)
+{
+  wnd_node* node = wnds + lp;
+  RECT rect;
+  if(hwnd == node->hwnd || !IsWindowVisible(hwnd) || !GetWindowRect(hwnd, &rect)) return TRUE;
+
+  if(node->t < rect.bottom + DY && node->b > rect.top - DY)
+  {
+    int dl = rect.left - node->l;
+    if(abs(dl) < abs(node->dl)) node->dl = dl;
+    dl = rect.right - node->l;
+    if(abs(dl) < abs(node->dl)) node->dl = dl;
+
+    int dr = rect.left - node->r;
+    if(abs(dr) < abs(node->dr)) node->dr = dr;
+    dr = rect.right - node->r;
+    if(abs(dr) < abs(node->dr)) node->dr = dr;
+  }
+
+  if(node->l < rect.right + DX && node->r > rect.left - DX)
+  {
+    int dt = rect.top - node->t;
+    if(abs(dt) < abs(node->dt)) node->dt = dt;
+    dt = rect.bottom - node->t;
+    if(abs(dt) < abs(node->dt)) node->dt = dt;
+
+    int db = rect.top - node->b;
+    if(abs(db) < abs(node->db)) node->db = db;
+    db = rect.bottom - node->b;
+    if(abs(db) < abs(node->db)) node->db = db;
+  }
+
+  return TRUE;
+}
+
 LRESULT CALLBACK fproc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp, UINT_PTR id, DWORD_PTR data)
 {
-  if(msg != WM_WINDOWPOSCHANGING) goto ret;
-  _log_("fproc: msg = WM_WINDOWPOSCHANGING\n");
+  switch(msg)
+  {
+  case WM_WINDOWPOSCHANGING:
+    _log_("fproc: msg = WM_WINDOWPOSCHANGING\n");
+
+    PWINDOWPOS pos = (PWINDOWPOS)lp;
+    if(pos->flags & SWP_NOMOVE && pos->flags & SWP_NOSIZE) break;
+    _log_("  x = %d, y = %d, w = %d, h = %d\n", pos->x, pos->y, pos->cx, pos->cy);
+
+    RECT rect; // current window position
+    if(!GetWindowRect(hwnd, &rect)) break;
+
+    wnd_node* node = wnd_find(hwnd);
+    if(!node) break;
+
+    node->l = pos->x;
+    node->r = pos->x + pos->cx;
+    node->t = pos->y;
+    node->b = pos->y + pos->cy;
+
+    node->dl = node->dr = node->dt = node->db = INT_MAX;
+    EnumWindows(fenum, node - wnds);
+    _log_("  dl = %d, dr = %d, dt = %d, db = %d\n", node->dl, node->dr, node->dt, node->db);
+
+    if(!(pos->flags & SWP_NOMOVE) &&
+      pos->cx == (rect.right - rect.left) && pos->cy == (rect.bottom - rect.top) &&
+      (pos->x != rect.left || pos->y != rect.top))
+    {
+      int dx = node->dl;
+      if(abs(node->dr) < abs(dx)) dx = node->dr;
+      int dy = node->dt;
+      if(abs(node->db) < abs(dy)) dy = node->db;
+
+      if(abs(dx) < DX) pos->x = node->l + dx;
+      if(abs(dy) < DY) pos->y = node->t + dy;
+    }
+    else if(!(pos->flags & SWP_NOSIZE))
+    {
+      if(pos->cx != (node->r - node->l))
+      {
+      }
+      if(pos->cy != (node->b - node->t))
+      {
+      }
+    }
+
+    break;
+  }
 
   ret:
   return DefSubclassProc(hwnd, msg, wp, lp);
@@ -74,7 +157,7 @@ DLL_EXPORT LRESULT CALLBACK fhook(int code, WPARAM wp, LPARAM lp)
     _log_("fhook: msg = WM_EXITSIZEMOVE\n");
     if(!(node = wnd_find(hwnd))) break;
 
-    _log_("  remove subclass: hwnd = %08X\n, fproc = %08X", hwnd, fproc);
+    _log_("  remove subclass: hwnd = %08X, fproc = %08X\n", hwnd, fproc);
     RemoveWindowSubclass(hwnd, fproc, 0);
     memset(node, 0, sizeof(wnd_node));
     break;
