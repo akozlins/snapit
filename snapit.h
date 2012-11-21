00a34522daf095266c5618489bfc75b944bc8fec
Alexandr Kozlinskiy <akozlins@gmail.com>
2012-11-21 18:40:38 +0100

    add missing header

---
+++ snapit.h
@@ -0,0 +1,14 @@
+
+#if defined(WIN64)
+const char* g_mutex_name = "MUTEX_SNAPIT_x64_{494e0de4-493b-4d30-9eb5-e7de12b247c0}";
+const char* g_message_name = "WMU_SNAPIT_UNINSTALL_x64_{494e0de4-493b-4d30-9eb5-e7de12b247c0}";
+const char* g_class_name = "SnapIt_x64_{494e0de4-493b-4d30-9eb5-e7de12b247c0}";
+const char* g_title = "SnapItX64";
+const char* g_icon_name = "snapit_x64.ico";
+#else
+const char* g_mutex_name = "MUTEX_SNAPIT_x32_{494e0de4-493b-4d30-9eb5-e7de12b247c0}";
+const char* g_message_name = "WMU_SNAPIT_UNINSTALL_x32_{494e0de4-493b-4d30-9eb5-e7de12b247c0}";
+const char* g_class_name = "SnapIt_x32_{494e0de4-493b-4d30-9eb5-e7de12b247c0}";
+const char* g_title = "SnapItX32";
+const char* g_icon_name = "snapit_x32.ico";
+#endif
