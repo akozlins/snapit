eea2a25e88c56d1593cdb50415a9e34ec2a71457
Alexandr Kozlinskiy <akozlins@gmail.com>
2012-11-21 17:42:07 +0100

    minor fix

--- make_x32.bat
+++ make_x32.bat
@@ -1,11 +1,13 @@
 
 call "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat"
 
-@set CLOPTS=/nologo /W3 /MD /O2 /D "NDEBUG" /D "WIN32" /c
-@set LINKOPTS=/nologo /RELEASE /INCREMENTAL:NO /OPT:REF /OPT:ICF /NODEFAULTLIB msvcrt.lib kernel32.lib /MACHINE:X86
+@set CLOPTS=/nologo /W3 /MD /O2 /D "NDEBUG" /c
+@set LINKOPTS=/nologo /RELEASE /INCREMENTAL:NO /OPT:REF /OPT:ICF /NODEFAULTLIB msvcrt.lib kernel32.lib
 
-cl %CLOPTS% /Tp hook.cpp
-link %LINKOPTS% user32.lib gdi32.lib comctl32.lib psapi.lib /DLL /out:hook_x32.dll hook.obj
+del hook.obj
+cl %CLOPTS% /D "WIN32" hook.cpp
+link %LINKOPTS% user32.lib gdi32.lib comctl32.lib psapi.lib /DLL /out:hook_x32.dll hook.obj /MACHINE:X86
 
-cl %CLOPTS% /Tp snapit.cpp
-link %LINKOPTS% user32.lib gdi32.lib shell32.lib hook_x32.lib /out:snapit_x32.exe snapit.obj /SUBSYSTEM:WINDOWS
+del snapit.obj
+cl %CLOPTS% /D "WIN32" snapit.cpp
+link %LINKOPTS% user32.lib gdi32.lib shell32.lib hook_x32.lib /out:snapit_x32.exe snapit.obj /MACHINE:X86 /SUBSYSTEM:WINDOWS
--- make_x64.bat
+++ make_x64.bat
@@ -1,11 +1,13 @@
 
 call "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat" amd64
 
-@set CLOPTS=/nologo /W3 /MD /O2 /D "NDEBUG" /D "WIN64" /c
-@set LINKOPTS=/nologo /RELEASE /INCREMENTAL:NO /OPT:REF /OPT:ICF /NODEFAULTLIB msvcrt.lib kernel32.lib /MACHINE:X64
+@set CLOPTS=/nologo /W3 /MD /O2 /D "NDEBUG" /c
+@set LINKOPTS=/nologo /RELEASE /INCREMENTAL:NO /OPT:REF /OPT:ICF /NODEFAULTLIB msvcrt.lib kernel32.lib
 
-cl %CLOPTS% /Tp hook.cpp
-link %LINKOPTS% user32.lib gdi32.lib comctl32.lib psapi.lib /DLL /out:hook_x64.dll hook.obj
+del hook.obj
+cl %CLOPTS% /D "WIN64" hook.cpp
+link %LINKOPTS% user32.lib gdi32.lib comctl32.lib psapi.lib /DLL /out:hook_x64.dll hook.obj /MACHINE:X64
 
-cl %CLOPTS% /Tp snapit.cpp
-link %LINKOPTS% user32.lib gdi32.lib shell32.lib hook_x64.lib /out:snapit_x64.exe snapit.obj /SUBSYSTEM:WINDOWS
+del snapit.obj
+cl %CLOPTS% /D "WIN64" snapit.cpp
+link %LINKOPTS% user32.lib gdi32.lib shell32.lib hook_x64.lib /out:snapit_x64.exe snapit.obj /MACHINE:X64 /SUBSYSTEM:WINDOWS
