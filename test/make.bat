996e4c5ca75e56b8777c7d23a107fa952412cbd3
Alexandr Kozlinskiy <akozlins@gmail.com>
2012-11-21 20:05:12 +0100

    minor change

--- test/make.bat
+++ test/make.bat
@@ -1,8 +1,8 @@
 
 call "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat"
 
-@set CLOPTS=/nologo /W3 /MD /O2 /D "NDEBUG" /D "WIN32" /c
-@set LINKOPTS=/nologo /RELEASE /INCREMENTAL:NO /OPT:REF /OPT:ICF /NODEFAULTLIB msvcrt.lib kernel32.lib /MACHINE:X86
+@set CLOPTS=/nologo /W3 /MD /O2 /D "NDEBUG" /c
+@set LINKOPTS=/nologo /RELEASE /INCREMENTAL:NO /OPT:REF /OPT:ICF /NODEFAULTLIB msvcrt.lib kernel32.lib
 
-cl %CLOPTS% /Tp winrgn.cpp
-link %LINKOPTS% user32.lib gdi32.lib shell32.lib /out:winrgn.exe winrgn.obj /SUBSYSTEM:CONSOLE
+cl %CLOPTS% /D "WIN32" winrgn.cpp
+link %LINKOPTS% user32.lib gdi32.lib shell32.lib /out:winrgn.exe winrgn.obj /MACHINE:X86 /SUBSYSTEM:CONSOLE
